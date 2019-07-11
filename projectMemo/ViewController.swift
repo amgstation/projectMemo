b4f9a1be2011590c8a900c38c49863fa2981efd7
amgstation <amgstation@naver.com>
2019-07-11 20:36:42 +0900

    Initial Commit

---
+++ projectMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  projectMemo
+//
+//  Created by amgstation on 11/07/2019.
+//  Copyright © 2019 amgstation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
